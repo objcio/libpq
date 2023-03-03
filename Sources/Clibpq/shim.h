dc3301f230face2f30ecd51eadf975763803a758
Chris Eidhof <chris@eidhof.nl>
2023-03-03 11:46:44 +0100

    Separate includes

--- Sources/Clibpq/shim.h
+++ Sources/Clibpq/shim.h
@@ -1,7 +1,16 @@
 #ifndef __CPOSTGRESQL_SHIM_H__
 #define __CPOSTGRESQL_SHIM_H__
 
+#ifdef __APPLE__
+
+#include "/opt/homebrew/opt/libpq/includA/libpq-fe.h"
+#include "/opt/homebrew/opt/libpq/includA/postgres_ext.h"
+
+#else
+
 #include <libpq-fe.h>
 #include <postgres_ext.h>
+
+#endif
 
 #endif
